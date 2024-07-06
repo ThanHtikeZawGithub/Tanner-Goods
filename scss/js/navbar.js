7cb059a3f5c72afd2e9fe8bb2a78291cdfc56969
ThanHtikeZaw1 <thanhtikezaw@genkisystem.com>
2024-07-06 10:22:11 +0630

    update

--- scss/js/navbar.js
+++ scss/js/navbar.js
@@ -15,3 +15,27 @@
     });
   });
 })(jQuery);
+
+document.addEventListener('DOMContentLoaded', function() {
+  const navLinks = document.querySelectorAll('.nav-link');
+  const sections = document.querySelectorAll('section');
+
+  function setActiveLink() {
+    let index = sections.length;
+
+    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+    navLinks.forEach((link) => link.classList.remove('active'));
+    navLinks[index].classList.add('active');
+  }
+
+  setActiveLink();
+  window.addEventListener('scroll', setActiveLink);
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+      navLinks.forEach(link => link.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+});
